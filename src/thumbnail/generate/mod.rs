ad1bd3a7756a632b76fc6bce340dce4679928b2f
Matt Fellenz <matt@felle.nz>
2022-09-06 13:16:27 -0700

    Basis

--- src/thumbnail/generate/mod.rs
+++ src/thumbnail/generate/mod.rs
@@ -18,13 +18,78 @@ fn initialize_gst() {
 	scale_plugin::plugin_register_static().unwrap();
 }
 
+#[derive(Debug)]
+enum FrameError {
+	EndOfStream,
+	GstError {
+		src: Option<String>,
+		error: String,
+		debug: Option<String>,
+	},
+}
+
+struct PipelineWrapper(pub gst::Pipeline);
+
+impl std::ops::Deref for PipelineWrapper {
+	type Target = gst::Pipeline;
+
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
+
+impl std::ops::DerefMut for PipelineWrapper {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.0
+	}
+}
+
+impl Drop for PipelineWrapper {
+	fn drop(&mut self) {
+		self.0.set_state(gst::State::Null).unwrap();
+	}
+}
+
 #[tracing::instrument]
 pub(in crate::thumbnail) fn generate(input: &Path, mut output: &File) -> Result<(), GenerateError> {
 	GST_INIT.get_or_init(initialize_gst);
 
-	let (frame_send, frame_recv) = std::sync::mpsc::sync_channel(1);
+	let (frame_send, frame_recv) = std::sync::mpsc::sync_channel::<Result<Vec<u8>, FrameError>>(1);
 
-	let pipeline = create_pipeline(input);
+	let pipeline = PipelineWrapper(create_pipeline(input));
+
+	std::thread::spawn({
+		let frame_send = frame_send.clone();
+		let bus = pipeline.bus().unwrap();
+		move || {
+			// timed_pop with None for the time blocks until there's a message
+			while let Some(message) = bus.timed_pop(None) {
+				match message.view() {
+					gst::MessageView::Eos(..) => {
+						tracing::trace!("received EOS message, sending None");
+						frame_send.try_send(Err(FrameError::EndOfStream)).unwrap();
+						break;
+					}
+					gst::MessageView::Error(error) => {
+						tracing::error!("gstreamer bus error: {error:?}");
+						frame_send
+							.try_send(Err(FrameError::GstError {
+								src: error.src().map(|src| src.to_string()),
+								error: error.error().to_string(),
+								debug: error.debug(),
+							}))
+							.unwrap();
+						break;
+					}
+					gst::MessageView::Warning(warning) => {
+						tracing::warn!("gstreamer bus warning: {warning:?}");
+					}
+					_ => (),
+				}
+			}
+		}
+	});
+
 	let sink = pipeline
 		.by_name("sink")
 		.unwrap()
@@ -40,7 +105,7 @@ pub(in crate::thumbnail) fn generate(input: &Path, mut output: &File) -> Result<
 					let sample = sink.pull_sample().unwrap();
 					let buffer_ref = sample.buffer().unwrap();
 					let buffer = buffer_ref.map_readable().unwrap();
-					let _ = frame_send.send(Some(buffer.to_vec()));
+					frame_send.try_send(Ok(buffer.to_vec())).unwrap();
 					// stop after one frame
 					Err(gst::FlowError::Eos)
 				}
@@ -49,36 +114,12 @@ pub(in crate::thumbnail) fn generate(input: &Path, mut output: &File) -> Result<
 				let frame_send = frame_send.clone();
 				move |_sink| {
 					tracing::trace!("end-of-stream callback called");
-					let _ = frame_send.send(None);
+					frame_send.try_send(Err(FrameError::EndOfStream)).unwrap();
 				}
 			})
 			.build(),
 	);
-
-	pipeline
-		.bus()
-		.unwrap()
-		.add_watch(move |_bus, message| {
-			let keep_closure = match message.view() {
-				gst::MessageView::Eos(..) => {
-					tracing::trace!("received EOS message, sending None");
-					let _ = frame_send.send(None);
-					false
-				}
-				gst::MessageView::Error(error) => {
-					tracing::error!("gstreamer bus error: {error:?}");
-					let _ = frame_send.send(None);
-					false
-				}
-				gst::MessageView::Warning(warning) => {
-					tracing::warn!("gstreamer bus warning: {warning:?}");
-					true
-				}
-				_ => true,
-			};
-			gst::prelude::Continue(keep_closure)
-		})
-		.unwrap();
+	drop(frame_send);
 
 	tracing::trace!("starting pipeline");
 	pipeline.set_state(gst::State::Playing).unwrap();
@@ -87,9 +128,13 @@ pub(in crate::thumbnail) fn generate(input: &Path, mut output: &File) -> Result<
 	let frame = frame_recv
 		.recv_timeout(Duration::from_secs(5))
 		.map_err(|_| GenerateError::Custom("thumbnail generation timed out"))?
-		.ok_or(GenerateError::Custom("video has no frames"))?;
-
-	pipeline.set_state(gst::State::Null).unwrap();
+		.map_err(|error| match error {
+			FrameError::EndOfStream => GenerateError::Custom("video has no frames"),
+			FrameError::GstError { .. } => {
+				tracing::error!("gst error: {error:?}");
+				GenerateError::Custom("gstreamer error")
+			}
+		})?;
 
 	tracing::trace!("writing frame to output");
 	output.write_all(&frame).map_err(io_ctx("writing output"))?;
